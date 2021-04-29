ecddd01001936515cfce4e5b145e635f2292b528
shihaji <shihajig@gmail.com>
2021-04-29 09:59:30 +0530

    2 commit

--- TestProject/src/main/java/com/cts/Calculator.java
+++ TestProject/src/main/java/com/cts/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 	public int addMethod(int a,int b) {
 		
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- TestProject/src/test/java/com/cts/CalculatorTest.java
+++ TestProject/src/test/java/com/cts/CalculatorTest.java
@@ -2,69 +2,55 @@ package com.cts;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class CalculatorTest {
 	
+	int input1;
+	int input2;
+	int output;
+	
+	public CalculatorTest(int input1,int input2, int output) {
+		this.input1=input1;
+		this.input2=input2;
+		this.output=output;
+	}
+
+	@Parameters
+	public static Collection testDatas() {
+		
+		return Arrays.asList(new Object[][] {{5,5,15},{10,10,20},{3,3,6}});
+		
+	}
+	
+	
 	Calculator calc=null;
-
+	
 	@Before
-	public void setUp() throws Exception {  // precondition
-		
-	calc=new Calculator();
-	
-	}
-			
-                                                        
-	@Test(timeout = 4000)
-	public void testAddMethod() {                          
-                 	  
-            int result=calc.addMethod(5, 5);
-            
-            assertEquals(10, result);
-	}
-	
-	@Test
-	public void testAddMethodNegative() {                          
-                 	  
-            int result=calc.addMethod(5, 5);
-            
-            assertNotEquals(100, result);
+	public void setUp() throws Exception {
+		calc=new Calculator();
 	}
 
 	
-	
+
 	@Test
-	public void testMultiplyMethod() {
-		assertEquals(50,calc.multiplyMethod(10, 5));
-	}
+	public void testAddMethod() {
 	
-	@Test
-    public void testdivideMethod() {
+		assertEquals(output,calc.addMethod(input1, input2));
 		
-    	assertEquals(2,calc.divideMethod(10, 5));
 	}
-	
-	@Test(expected = ArithmeticException.class)
-    public void testdivideMethod2() {
-		
-    	calc.divideMethod(10, 0);
-	}
-	
 	
 	@After
-	public void tearDown() throws Exception { // fixtures  postcondition
-		
-		calc=null;
+	public void tearDown() throws Exception {
 	}
-	
-	
-	
-	
 
 }
